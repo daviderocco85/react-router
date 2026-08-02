a3fbb277c6bba7f34226bb2059d04735cb3c375a
daviderocco85 <roccodavide85@gmail.com>
2026-08-02 12:57:41 +0200

    feat: add AboutUs page with company description

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,10 @@
+export const AboutUs = () => {
+    return (
+        <div className='about-us page'>
+            Siamo uno store online specializzato
+            nella vendita di prodotti di vario genere da molti anni.
+            Abbiamo prezzi super convenienti, provare per credere!
+        </div>
+    );
+
+};
