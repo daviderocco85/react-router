791fbae6461d48cdb9ed3b1344f7c72bb0751c65
daviderocco85 <roccodavide85@gmail.com>
2026-08-02 12:52:14 +0200

    feat: configure application routes with ShellLayout and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css';
+import { ShellLayout } from './components/ShellLayout';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { AboutUs } from './pages/AboutUs';
+import { Products } from './pages/Products';
+
+
 
 
 export const App = () => {
 
   return (
-    <>
-      Works
-    </>
+    <Routes>
+      <Route element={<ShellLayout />}>
+        <Route index element={<Home />} />
+        <Route path='about-us' element={<AboutUs />} />
+        <Route path='products' element={<Products />} />
+      </Route>
+    </Routes>
   );
 };
 
