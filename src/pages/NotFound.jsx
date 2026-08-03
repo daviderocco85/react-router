c37a9f8b3d5ce5b52aece5a01ab6f1bd582ea39d
daviderocco85 <roccodavide85@gmail.com>
2026-08-03 17:07:45 +0200

    feat: add NotFound page for unmatched routes

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+    return (
+        <div className="page">
+            <h1>404 - Pagina non trovata</h1>
+            <p>La pagina che stai cercando non esiste.</p>
+        </div>
+    );
+};
